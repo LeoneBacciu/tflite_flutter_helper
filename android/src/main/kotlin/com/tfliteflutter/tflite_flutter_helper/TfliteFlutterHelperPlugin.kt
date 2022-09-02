cadba186798a1cf2d29f19dde480d7add453e959
Leone Bacciu <leonebacciu@gmail.com>
2022-09-02 12:25:13 +0200

    Update TfliteFlutterHelperPlugin.kt

--- android/src/main/kotlin/com/tfliteflutter/tflite_flutter_helper/TfliteFlutterHelperPlugin.kt
+++ android/src/main/kotlin/com/tfliteflutter/tflite_flutter_helper/TfliteFlutterHelperPlugin.kt
@@ -140,12 +140,13 @@ class TfliteFlutterHelperPlugin : FlutterPlugin,
 		}
 	}
 
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
-											grantResults: IntArray): Boolean {
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?,
+											grantResults: IntArray?): Boolean {
 		when (requestCode) {
 			AUDIO_RECORD_PERMISSION_CODE -> {
-				permissionToRecordAudio = grantResults.isNotEmpty() &&
-						grantResults[0] == PackageManager.PERMISSION_GRANTED
+				if (grantResults != null) {
+					permissionToRecordAudio = grantResults.isNotEmpty() &&
+							grantResults[0] == PackageManager.PERMISSION_GRANTED
 				}
 				completeInitializeRecorder()
 				return true
